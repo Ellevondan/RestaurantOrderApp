dc5d1e3280e562cc02736ab55b7f179a693b0026
dada2200 <143810211+dada2200@users.noreply.github.com>
2025-12-14 15:31:12 +0100

    implement fetching grouporders, sum up the price and removing group when payment/checkout=complete

--- app/src/main/java/com/miun/restaurantorderapp/models/ModifiedItem.java
+++ app/src/main/java/com/miun/restaurantorderapp/models/ModifiedItem.java
@@ -4,7 +4,10 @@ import com.google.gson.annotations.SerializedName;
 
 public class ModifiedItem {
     @SerializedName("id")
-    private Long id;  // MenuItem ID
+    private Long id;
+
+    @SerializedName("originalID")
+    private Long originalId;  // MenuItem ID
 
     @SerializedName("name")
     private String name;
@@ -24,19 +27,29 @@ public class ModifiedItem {
     @SerializedName("orderedAt")
     private String orderedAt;  // "2025-12-11T12:34:56"
 
+    private transient Double price;
+
     // Getters o setters
 
     public void setId(Long id) {
         this.id = id;
     }
+    public Long getId() { return id; }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public void setOriginalId(Long originalId) { this.originalId = originalId; }
+    public Long getOriginalId() { return originalId; }
 
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
+    public void setName(String name) { this.name = name; }
+    public String getName() { return name; }
+
+
+    public void setQuantity(Integer quantity) { this.quantity = quantity; }
+    public Integer getQuantity() { return quantity; }
+
+    public void setPrice(Double price) { this.price = price; }
+    public Double getPrice() { return price; }
+
+
 
     public void setSelectedAllergens(String selectedAllergens) {
         this.selectedAllergens = selectedAllergens;
